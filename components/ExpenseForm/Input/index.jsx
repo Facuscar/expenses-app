9445d5e8462d2f07190d24b340f8025d8673bb10
facuscar <facuscardillo@gmail.com>
2024-10-19 22:16:57 -0300

    Add styling

--- components/ExpenseForm/Input/index.jsx
+++ components/ExpenseForm/Input/index.jsx
@@ -1,12 +1,36 @@
-import { Text, TextInput, View } from "react-native"
+import { StyleSheet, Text, TextInput, View } from "react-native"
+import { GlobalStyles } from "../../../constants/styles";
 
 const Input = ({ label, textInputProps }) => {
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputProps} />
+    <View style={styles.inputWrapper}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={[styles.input, textInputProps.multiline && styles.multilineInput]} {...textInputProps} />
     </View>
   )
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: GlobalStyles.colors.primary700,
+  },
+  multilineInput: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  }
+})
